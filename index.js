fe4c996c780862d93346310f027899b8c93b6a0b
Marc Nopprapun <mnopprapun@gmail.com>
2021-04-07 09:06:47 -0500

    commit tree ecf4b22612bb09268dfd944fe001c55e24e0eb71

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Your code here
+function saturdayFun(activity = 'roller-skate'){
+    return `This Saturday, I want to ${activity}!`
+}
+function mondayWork(activity = 'go to the office'){
+return `This Monday, I will ${activity}.`}
+
+function wrapAdjective(adj = '*'){
+return function(adjective='special'){
+    return `You are ${adj}${adjective}${adj}!`
+}
+}
